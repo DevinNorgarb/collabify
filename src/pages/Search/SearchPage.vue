c622707cc9ffca97e2636635bab94d50bf9cd853
Devin <dnorgarb@gmail.com>
2020-04-27 04:02:25 +0200

    wip

--- src/pages/Search/SearchPage.vue
+++ src/pages/Search/SearchPage.vue
@@ -1,40 +1,4 @@
 <template>
-  <!-- <q-page padding>
-\    <q-page class="q-pa-md body-class">
-      <template v-if="data">
-        <dynamic-table
-          :title="'Search Music'"
-          :data="data"
-          :columns="columns"
-          :actions="['action 1', 'action 2', 'action 3']"
-          :search-field="filter"
-          :inner-loading="loading"
-          theme="secondary"
-          @search-change="filter = $event"
-          :selection-mode="selectionMode"
-          @selection-mode-change="
-          selectionMode =  'single'
-          modeChanged()
-        "
-          @action-1="doAction"
-          @action-2="doAction"
-          :grid="$q.platform.is.mobile"
-          actions-display-type="bottom menu"
-          action-invoke-type="instant"
-          search-input-placeholder="Search Name"
-          search
-          grid-card-style="vertical"
-          :row-options="[5, 10, 20, 30, 40, 50]"
-          :pagination="pagination"
-          ref="table"
-          @update:pagination="pagChange"
-        />
-      </template>
-      <template v-else>
-        <span>No data, do something like fetch data from server etc...</span>
-      </template>
-    </q-page>
-  </q-page> -->
 
   <q-page>
     <div class="q-pa-md">
@@ -64,7 +28,14 @@
         virtual-scroll
         :pagination.sync="pagination"
         :rows-per-page-options="[0]"
-      ></q-table>
+      >
+        <template v-slot:body-cell-actions="props">
+          <q-td :props="props">
+            <q-btn dense round flat color="grey" @click="play()" icon="pause"></q-btn>
+            <q-btn dense round flat color="grey" @click="deleteRow(props)" icon="play_arrow"></q-btn>
+          </q-td>
+        </template>
+      </q-table>
     </div>
   </q-page>
 </template>
@@ -83,61 +54,20 @@ export default {
   watch: {
     tracks: function (val) {
 
-        console.log("track changes");
-
       var test = Object.assign({}, val)
-
       console.log(test[0].name);
-
       this.dataTracks = []
-      // this.dataTracks.push(test)
-
-      // console.log(val);
-      // this.$nextTick(() => {
-      //   this.$set(this.dataTracks, "tracks", val)
-      // })
-
-        // this.$set(this.dataTracks, "tracks", val)
-
-        // this.dataTracks = val
-
-      //   this.$set(this.dataTracks, val)
-      // this.$nextTick(() => {
-      //   this.$set(this.prefixes[key], 'rpki', '1')
-
-      // })
-
-      // console.log("-----------------");
-
-      // // console.log(val);
-      // dataTracksArray = []
-
-      //   //  = val
-      // this.dataTracks = Object.assign({}, val)
       for (let index = 0; index < val.length; index++) {
         const element = val[index];
-
         this.dataTracks.push(element)
-        // this.$nextTick(function () {
-        //   this.$set(this.dataTracks[index], 'name', element.name)
-        //   this.$set(this.dataTracks[index], 'id', element.id)
-        // })
       }
-
-
-
     },
     filter: function (val, oldval) {
       this.$store.dispatch('spotifyAuth/search', val)
-      this.$nextTick(() => {
-        this.$set(this.dataTracks, "tracks", val)
-      })
-  // this.
   }
   },
   computed: {
     tracks () {
-      // this.$set(this.dataTracksArray, "tracks", ...this.$store.state.spotifyAuth.search_tracks.items)
       return this.$store.state.spotifyAuth.search_tracks.items
     }
   },
@@ -145,6 +75,10 @@ export default {
     console.log(this.$store.state.spotifyAuth.user.accessToken);
   },
   methods: {
+    play() {
+      this.$store.dispatch('spotifyAuth/getToken')
+
+    },
     modeChanged () {
       console.log('modechanged', this.selectionMode)
     },
@@ -178,15 +112,16 @@ export default {
           // format: val => `${val}`,
           sortable: true
         },
-        {
-          name: 'id',
-          align: 'center',
-          label: 'id',
-          field: 'id',
-          sortable: true,
-          // isRating: false,
-          // style: 'width: 150px'
-        },
+        // {
+        //   name: 'id',
+        //   align: 'center',
+        //   label: 'id',
+        //   field: 'id',
+        //   sortable: true,
+        //   // isRating: false,
+        //   // style: 'width: 150px'
+        // },
+         { name: 'actions', label: 'Actions', field: '', align:'center' },
         {
           name: 'artist',
           align: 'center',
@@ -201,20 +136,7 @@ export default {
 
       ],
       data: this.dataTracks
-      // || [
-      //   {
-      //     name: 'Frozen Yogurt',
-      //     calories: 159,
-      //     fat: 6.0,
-      //     carbs: 24,
-      //     protein: 4.0,
-      //     sodium: 87,
-      //     calcium: '14%',
-      //     iron: '1%',
-      //     rating: 1
-      //   }
 
-      // ]
     }
   },
 

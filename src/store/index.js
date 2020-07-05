59bc042cc517332cdc6323f5fc85a2c6b862782d
Devin <dnorgarb@gmail.com>
2020-07-05 22:50:39 +0200

    wip

--- src/store/index.js
+++ src/store/index.js
@@ -12,51 +12,51 @@ Vue.use(Vuex);
  * directly export the Store instantiation
  */
 
-export default function(/* { ssrContext } */) {
-  const Store = new Vuex.Store({
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    strict: process.env.DEV,
-    modules: modules,
-    // global actions
-    actions: {
-      // reset all states
-      resetAllState({ dispatch }) {
-        for (const currentModule in modules) {
-          if (modules[currentModule].state.hasOwnProperty("initState")) {
-            dispatch("resetModuleState", currentModule);
-          }
+// export default function(/* { ssrContext } */) {
+const Store = new Vuex.Store({
+  // enable strict mode (adds overhead!)
+  // for dev mode only
+  strict: process.env.DEV,
+  modules: modules,
+  // global actions
+  actions: {
+    // reset all states
+    resetAllState({ dispatch }) {
+      for (const currentModule in modules) {
+        if (modules[currentModule].state.hasOwnProperty("initState")) {
+          dispatch("resetModuleState", currentModule);
         }
-      },
-      // reset state by module
-      resetModuleState: ({ commit }, currentModule) => {
-        const initState = modules[currentModule].state.initState;
-        commit("RESET_STATES", {
-          namespace: currentModule,
-          initState: initState
-        });
       }
     },
-    mutations: {
-      // gobal reset state mutation
-      RESET_STATES(state, module) {
-        const initState = module.initState();
-        const namespace = module.namespace;
-        Object.keys(state[namespace]).forEach(key => {
-          // initialize state to og state
-          key !== "initState" && (state[namespace][key] = initState[key]);
-        });
-      }
-    },
-    plugins: [createPersistedState()]
+    // reset state by module
+    resetModuleState: ({ commit }, currentModule) => {
+      const initState = modules[currentModule].state.initState;
+      commit("RESET_STATES", {
+        namespace: currentModule,
+        initState: initState
+      });
+    }
+  },
+  mutations: {
+    // gobal reset state mutation
+    RESET_STATES(state, module) {
+      const initState = module.initState();
+      const namespace = module.namespace;
+      Object.keys(state[namespace]).forEach(key => {
+        // initialize state to og state
+        key !== "initState" && (state[namespace][key] = initState[key]);
+      });
+    }
+  },
+  plugins: [createPersistedState()]
+});
+
+if (process.env.DEV && module.hot) {
+  module.hot.accept(["features/stores-index.js"], () => {
+    const newModules = require("features/stores-index.js").default;
+    Store.hotUpdate({ modules: newModules });
   });
-
-  if (process.env.DEV && module.hot) {
-    module.hot.accept(["features/stores-index.js"], () => {
-      const newModules = require("features/stores-index.js").default;
-      Store.hotUpdate({ modules: newModules });
-    });
-  }
-
-  return Store;
 }
+
+export default Store;
+// }
